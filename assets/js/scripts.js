203b29f2bae37bc3fb97f765599fe69278080562
Lincoln Silva <106344749+Lincvasc@users.noreply.github.com>
2023-09-16 17:47:39 -0300

    atualização Lincoln
    Atualizados HTML CSS e JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,88 @@
+//Dark mode...
 const chk = document.getElementById("chk");
 
 chk.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
+//...Dark mode
 
-// Adicionar tarefas .....
-const tarefas = [];
+// Adicionar tarefas...
+let tarefas = [];
+const errorMessage = document.querySelector(".error-message");
 
 document.querySelector(".addbutton").addEventListener("click", () => {
-  const tarefa = document.querySelector(".input1").value;
+  adicionarTarefa();
+});
 
+document.querySelector(".to-do").addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    adicionarTarefa();
+  }
+});
+
+function adicionarTarefa() {
+  const tarefa = document.querySelector(".to-do").value;
+
+  if (tarefa.trim() === "") {
+    errorMessage.style.display = "block";
+    return;
+  }
+
+  errorMessage.style.display = "none";
   tarefas.push(tarefa);
 
+  const divTaskBox = document.querySelector(".taskbox").cloneNode(true);
   const paragrafo = document.createElement("p");
   paragrafo.textContent = tarefa;
 
-  document.querySelector(".taskbox").appendChild(paragrafo);
+  divTaskBox.classList.add("added");
+  divTaskBox.setAttribute("draggable", true);
 
-  document.querySelector(".input1").value = "";
+  divTaskBox.querySelector(".task").remove();
+  divTaskBox.querySelector(".task-content").appendChild(paragrafo);
+
+  document.querySelector(".tasks").appendChild(divTaskBox);
+
+  document.querySelector(".to-do").value = "";
+}
+// ...Adicionar tarefas
+
+
+//Mover tarefas...
+const columns = document.querySelectorAll(".column");
+
+document.addEventListener("dragstart", (e) => {
+  e.target.classList.add("dragging");
 });
-// .....Adicionar tarefas 
+
+document.addEventListener("dragend", (e) => {
+  e.target.classList.remove("dragging");
+});
+
+columns.forEach((item) => {
+  item.addEventListener("dragover", (e) => {
+    const dragging = document.querySelector(".dragging");
+    const applyAfter = getNewPosition(item, e.clientY);
+
+    if (applyAfter) {
+      applyAfter.insertAdjacentElement("afterend", dragging);
+    } else {
+      item.prepend(dragging);
+    }
+  });
+});
+
+function getNewPosition(column, posY) {
+  const cards = column.querySelectorAll(".item:not(.dragging)");
+  let result;
+
+  for (let refer_card of cards) {
+    const box = refer_card.getBoundingClientRect();
+    const boxCenterY = box.y + box.height / 2;
+
+    if (posY >= boxCenterY) result = refer_card;
+  }
+
+  return result;
+}
+//...Mover tarefas
